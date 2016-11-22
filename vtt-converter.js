b17892a002917b353c510b673fd65e92444d7ce5
Dillon Kerr <dkerr@mindedge.com>
2016-11-22 11:56:01 -0500

    Import file system package, create and export convert function which reads in file and will convert it

--- vtt-converter.js
+++ vtt-converter.js
@@ -1,3 +1,17 @@
 /**
  * Converter for XML files to VTT format
  */
+import fs from 'fs';
+
+function convert() {
+  console.log("Beginning Conversion...");
+  const file = fs.readFile('package.json', 'utf8', (err, data) => {
+    if(err) throw err;
+
+    data = data.trim();
+  });
+}
+
+export default {
+  convert
+}

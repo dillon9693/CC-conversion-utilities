39aa18db3db2e934917b6e76b4b5041d487e9a9d
Dillon Kerr <dkerr@mindedge.com>
2016-11-22 15:37:07 -0500

    Add all lines with a begin attribute and a 'hide_from_transcript' class to an array

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ function separateByLine(str) {
 }
 
 export function convert(str) {
-  console.log("Beginning Conversion...");
   const dataArr = separateByLine(str.trim());
 
-  return dataArr.join("\n");
-}
+  return 'WEBVTT\n\n' + dataArr.filter((line, index) => {
+    // get all lines that contain a begin attribute and have the 'hide_from_transcript class'
+    return line.indexOf('begin="') > -1 && line.indexOf('class="hide_from_transcript"') > -1;
+  });
+};
 
 export default {
   convert
-}
+};

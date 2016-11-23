2e1ec5c1272fa7566c4034d77a88f8f22001cdee
Dillon Kerr <dkerr@mindedge.com>
2016-11-23 10:58:23 -0500

    Create example p tags using VTT timestamps and text

--- vtt2xml.js
+++ vtt2xml.js
@@ -1,3 +1,16 @@
 export function vtt2xml(data) {
-  return "";
+  return data.filter((line) => {
+    // filter out empty lines and first line
+    return line.length > 0 && line.toUpperCase() !== 'WEBVTT';
+  }).map((line, index, arr) => {
+    // each vtt caption is 2 lines so skip second line
+    if(index % 2 === 1) {
+      return;
+    }
+
+    var timeArr = line.trim().replace(/\s+/, ' ').split(' '),
+        text = arr[index + 1];
+
+    return `      <p begin="${timeArr[0]}" end="${timeArr[2]}" class="hide_from_transcript">${text}</p>`;
+  }).join('\n');
 };

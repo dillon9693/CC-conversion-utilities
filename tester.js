3bb85233934feaa81fe7c120abab8e2650388c74
Dillon Kerr <dkerr@mindedge.com>
2016-11-23 11:06:13 -0500

    Create options object and add as param to convert function, make default read/write filenames using option input/output

--- tester.js
+++ tester.js
@@ -2,21 +2,21 @@ import fs from 'fs';
 
 import { convert } from './index';
 
-const output = 'output.vtt';
+const options = {input: 'vtt', output: 'xml'};
 
-const file = fs.readFile('test.xml', 'utf8', (err, data) => {
+fs.readFile(`test.${options.input}`, 'utf8', (err, data) => {
   if(err) throw err;
+  const outputData = convert(data, options);
 
-  const outputData = convert(data, {input: 'vtt', output: 'xml'});
   if(!outputData) {
     console.log('File could not be written.');
     return;
   }
 
   // write the file to output
-  fs.writeFile(__dirname + `/${output}`, outputData, function(err) {
+  fs.writeFile(__dirname + `/output.${options.output}`, outputData, function(err) {
     if(err) throw err;
 
-    console.log(`The file was saved to ${output}.`);
+    console.log(`The file was saved to output.${options.output}.`);
   });
 });
